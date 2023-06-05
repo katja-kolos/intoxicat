a0460f4a06026fcad616886d53b2638102662581
st185989 <st185989@stud.uni-stuttgart.de>
2023-06-05 10:46:32 +0200

    Update intoxicat_evaluation.py

--- intoxicat_evaluation.py
+++ intoxicat_evaluation.py
@@ -8,19 +8,26 @@ predicted_labels = ["a", "na"]
 # what I wanted to do here is checking everytime there's "alc" assign "a" as intox and "na" as not_intox
 # does it make sense? 
 
+def calculate_accuracy(ground_truth_labels, predicted_labels):
+  # Accuracy
+  accuracy = accuracy_score(ground_truth_labels, predicted_labels)
+  print("Accuracy: ", accuracy)
+  return accuracy
 
-# Accuracy
-accuracy = accuracy_score(ground_truth_labels, predicted_labels)
-print("Accuracy: ", accuracy)
+def calculate_precision(ground_truth_labels, predicted_labels):
+  # Precision 
+  precision = precision_score(ground_truth_labels, predicted_labels)
+  print("Precision: ", precision)
+  return precision
 
-# Precision 
-precision = precision_score(ground_truth_labels, predicted_labels)
-print("Precision: ", precision)
+def calculate_recall(ground_truth_labels, predicted_labels):
+  # Recall
+  recall = recall_score(ground_truth_labels, predicted_labels)
+  print("Recall: ", recall)
+  return recall
 
-# Recall
-recall = recall_score(ground_truth_labels, predicted_labels)
-print("Recall: ", recall)
-
-# F1 score
-f1 = f1_score(ground_truth_labels, predicted_labels)
-print("F1: ", f1)
+def calculate_f1(ground_truth_labels, predicted_labels):
+  # F1 score
+  f1 = f1_score(ground_truth_labels, predicted_labels)
+  print("F1: ", f1)
+  return f1
